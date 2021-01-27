ce1db0106fcdb16edc0ca505cb2f67eb4995d0ce
Farid Aboharb <aboharbf@gmail.com>
2021-01-26 21:52:42 -0500

    updated parse_data_NSx
    - made functional in phyzzy pipeline
    - updated output arguments and added detection of previously parsed files to the argument.

--- tools/parse_data_NSx.m
+++ tools/parse_data_NSx.m
@@ -1,9 +1,50 @@
-function parsed_chs = parse_data_NSx(filename,max_memo_GB,output_name,channels)
+function [outfile_path, parsed_chs] = parse_data_NSx(filename, max_memo_GB, output_name, channels)
 % Code only valid for recordings without pauses or data loss (Nsx.Data field can't be a cell)
 % This code requires the file openNSx.m, from the NPMK, in the path. You can download the download NPMK from https://github.com/BlackrockMicrosystems/NPMK/releases
 % max_memo_GB is an idea of the number of GB allocated for the data to be
 % stored in RAM, so it is used to compute the number of segments in which
 % the data should be split for processing
+
+% Determine if the files have already been parsed, by establishing the
+% conventional path and checking.
+[filepath, file, ~] = fileparts(filename);
+parsedFolder = [filepath '/' file '_parsed'];
+
+if ~exist('output_name','var') || isempty(output_name)
+    output_name = [parsedFolder '/' file '_NSX'];
+end
+
+outDirPath = dir(fullfile(parsedFolder, [file '_NSX_Ch*.NC5']));
+
+if 0%~isempty(outDirPath)
+  disp('Data already processed - returning appropriate paths')
+  outfile_path = fullfile({outDirPath.folder}, {outDirPath.name})';
+  return
+end
+
+% If the files don't exist, then proceed w/ parsing the input.
+tcum=0
+
+with_memory=true;
+try
+	memory;
+catch
+	with_memory=false;
+end
+if with_memory
+	[userview, systemview] = memory;
+	memo_avaible = floor(systemview.PhysicalMemory.Available*0.80);
+	if exist('max_memo_GB','var') && ~isempty(max_memo_GB)
+        max_memo = max_memo_GB*(1024)^3;
+		if max_memo > memo_avaible
+			error('max_memo_GB > 80% of Physical Memory Available')
+		end
+	else
+		max_memo = memo_avaible;
+	end
+else
+	max_memo = max_memo_GB*(1024)^3;
+end
 
 if ~exist('filename')
   filename = uigetfile('*.ns5');
@@ -19,36 +60,34 @@ end
 
 [filepath, file, fext] = fileparts(filename);
 parsedFolder = [filepath '/' file '_parsed'];
+mkdir(parsedFolder);
+
 fext = lower(fext(2:end));
 nsx_ext = fext(end);
 chext = ['.NC' nsx_ext];
 
 NSx = openNSx(filename, 'report','noread');
-nchan = NSx.MetaTags.ChannelCount;   % number of channels
-sr = NSx.MetaTags.SamplingFreq;   % sampling rate
+nchan = NSx.MetaTags.ChannelCount;    % number of channels
+sr = NSx.MetaTags.SamplingFreq;       % sampling rate
 lts = sum(NSx.MetaTags.DataPoints);   % total data points
-outfile_handles = cell(1,nchan); %some will be empty
+outfile_handles = cell(1,nchan);      % some will be empty
 
 if ~exist('output_name','var') || isempty(output_name)
   output_name = [parsedFolder '/' file '_NSX'];
 end
 
-% if ~isempty(outDirPath) MAYBE REDUNDENT NOW?
-%   disp('Data already processed - returning appropriate paths')
-%   outfile_path = fullfile({outDirPath.folder}, {outDirPath.name})';
-%   return
-% outDirPath = dir([parsedFolder '/'  file '_NSX_Ch*.NC5']);
-
-% else
-
+[outfile_path] = deal(cell(sum(NSx.MetaTags.ChannelID < 129),1));
 parsed_chs = [];
 for i = 1:nchan
   c = NSx.MetaTags.ChannelID(i);
   if ismember(c,channels)
     parsed_chs(end+1) = c;
-    outfile_handles{i} = fopen([output_name '_' num2str(c) chext],'w');
+    outfile_path{i} = [output_name '_Ch' num2str(c) '.NC5'];
+    outfile_handles{i} = fopen(outfile_path{i},'w');
   end
 end
+
+
 DataPoints = NSx.MetaTags.DataPoints;
 
 DataPoints = [0 DataPoints]; %added for use as starting index
@@ -71,17 +110,12 @@ for part = 2:length(DataPoints)
   end
   fprintf('Segment %d out of %d processed. Data Point Read = %d \n',j,num_segments,size(NSx.Data,2));
 end
-end
-fclose('all');
-tcum = tcum + toc;
-fprintf('Total time spent in parsing the data was %s secs.\n',num2str(tcum, '%0.1f'));
-end
 
 fclose('all');
 tcum = tcum + toc;
-fprintf('Total time spent in parsing the data was %s secs.\n',num2str(tcum, '%0.1f'));
+fprintf('Total time spent in parsing the data was %s secs.\n', num2str(tcum, '%0.1f'));
 
-metadata_file = fullfile(pwd, [output_name '_TimeStamps.mat']);
+metadata_file = [output_name '_TimeStamps.mat'];
 metadata = struct;
 if exist(metadata_file,'file')
   metadata = load(metadata_file);
